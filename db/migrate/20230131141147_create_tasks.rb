7e5a23e5aedb5b3e3b7db9ff089a5ebe36c71e6d
shihoimamura <zhibao29@yahoo.co.jp>
2023-02-03 15:53:56 +0900

    テーブル設計

--- db/migrate/20230131141147_create_tasks.rb
+++ db/migrate/20230131141147_create_tasks.rb
@@ -1,6 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.string :task_name
+      t.string :task_content
+      t.string :deadline
+      t.string :status
+      t.string :priority
+
 
       t.timestamps
     end
