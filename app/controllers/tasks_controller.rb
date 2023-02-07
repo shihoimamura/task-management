d485b10dbeb32cfd45e78529d9b0346b7198b181
shihoimamura <zhibao29@yahoo.co.jp>
2023-02-07 17:48:15 +0900

    設定変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,21 @@ class TasksController < ApplicationController
   end
 
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
