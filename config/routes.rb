eb059cdba515b302e47c2b7d3799df817270fc43
shihoimamura <zhibao29@yahoo.co.jp>
2023-02-10 21:37:42 +0900

    一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # get 'task/management'
   # get '/task', to: 'task#index' #追記する
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :tasks
 end
